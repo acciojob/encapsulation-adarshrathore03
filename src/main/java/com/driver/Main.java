ee857bf3e89110320d87c01f0ed99d86540fd687
Adarsh Rathore <rajputadarsh2@gmail.com>
2023-09-15 21:29:35 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+
+         //rw.name = "adarsh";
+        //it will not work because variable is private in other pachage
+
+        rw.setName("Adarsh");
+        rw.getName();
+
+    }
 }
